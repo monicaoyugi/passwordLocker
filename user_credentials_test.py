4f90b03b691e34c459389da53c2b1f4e53c79057
monicaoyugi <monicaoyugi@gmail.com>
2020-01-25 09:17:27 +0300

    added self setUp credentials

---
+++ user_credentials_test.py
@@ -0,0 +1,17 @@
+import unittest
+import user_credentials_test, user_credentials_test
+
+class TestUser(unittest.TestCase);
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+    unittest.TestCase: TestCase class that helps in creating test cases.
+    '''
+    
+    
+    def setUp(self):
+        '''
+        Set up method to run before each check if pyperclip is installedtest cases.
+        '''
+        self.new_user = User('Monica', 'Oyugi', 'ndito123') # create user object
